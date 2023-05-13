e0754e8c2f939bdb34af3a409ddb02079a093bc2
Ashwin Acharya <87590123+ashwin-acharya01@users.noreply.github.com>
2023-05-13 23:53:21 +0530

    Initial Commit
    Initial Commit for file structure and basic dependencies.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>This is App.js file</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>This is the Homepage</div>
+  )
+}
+
+export default Homepage
