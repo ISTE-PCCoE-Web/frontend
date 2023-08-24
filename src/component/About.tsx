3ed9a5f8c89c727c6f2201858306d02161f4abc6
Girish <girishbhosale345@gmail.com>
2023-08-24 15:52:47 +0530

    new chakra

---
+++ src/component/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './about.css';
+
+const AboutPage: React.FC = () => {
+  
+  return (
+    <div className="about-page">
+      <h2>ABOUT US</h2>
+      <p>We are a community of passionate students who are committed to advancing technology education and innovation. Our chapter is a part of the larger Indian Society for Technical Education (ISTE), which is a global organization dedicated to empowering educators to transform learning and teaching through the effective use of technology.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
