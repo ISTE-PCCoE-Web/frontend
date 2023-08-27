46aa6c8d01dbcb1f80532e47f198b710313e0979
Abhijeet-Bhushari <abhijeetvijaybhushari242002@gmail.com>
2023-08-27 19:01:14 +0530

    Dynamic Team page

--- src/component/Team/Card.tsx
+++ src/component/Team/Card.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+//@ts-nocheck
+import React from 'react';
+import "./card.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-export default function Card() {
+// The Card component receives a 'member' prop containing details about a team member
+export default function Card(props) {
   return (
-    <div>
-      
+    <div className="column">
+      <div className="card">
+        {/* Display the profile image */}
+        <div className="img-container">
+          <img src={props.member.image} alt="" />
+        </div>
+        
+        {/* Display the member's full name */}
+        <h3>{props.member.firstName + " " + props.member.lastName}</h3>
+        
+        {/* Display the member's position */}
+        <p>{props.member.position}</p>
+        
+        {/* Social media icons */}
+        <div className="icons">
+          {/* Display an email icon with a link if an email is available */}
+          <a href={props.member.socials && props.member.socials.email} className="icon">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          
+          {/* Display a LinkedIn icon with a link if a LinkedIn profile is available */}
+          <a href={props.member.socials && props.member.socials.linkedin} className="icon">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          
+          {/* Display a GitHub icon with a link if a GitHub profile is available */}
+          <a href={props.member.socials && props.member.socials.github} className="icon">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/component/Team/Team.tsx
+++ src/component/Team/Team.tsx
@@ -1,168 +1,29 @@
+//@ts-nocheck
 import React from 'react';
 import Card from './Card';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter, faGithub, faLinkedin, faGit } from '@fortawesome/free-brands-svg-icons';
 import "./team.css"
+import membersData from "./members.json"
 
+// The Team component displays a list of team members using the Card component
 export default function Team() {
-   let message = `There are many messages available for us to display here.`
-   return (
-    <div className="container">
-        <section>
-            <div className="row">
-                <h1>Our Team</h1>
-                <p>{message}</p>
-            </div>
-            <div className="row">
-            {/* Column 1 */}
-                <div className="column">
-                    <div className="card">
-                        <div className="img-container">
-                            <img src="profile-img-1.png" alt="" />
-                        </div>
-                        <h3>Luna Turner</h3>
-                        <p>Founder</p>
-                        <div className="icons">
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faEnvelope}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faTwitter}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faLinkedin}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faGithub}/>
-                            </a>
-                        </div>
-                    </div>
+    return (
+        <div className="container">
+            {/* Team section */}
+            <section>
+                {/* Heading */}
+                <div className="row">
+                    <h1>Our Team</h1>
                 </div>
-            {/* Column 2 */}
-                <div className="column">
-                    <div className="card">
-                        <div className="img-container">
-                            <img src="profile-img-1.png" alt="" />
-                        </div>
-                        <h3>James Clark</h3>
-                        <p>Developer</p>
-                        <div className="icons">
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faEnvelope}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faTwitter}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faLinkedin}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faGithub}/>
-                            </a>
-                        </div>
-                    </div>
+
+                {/* Displaying team members using the Card component */}
+                <div className="row">
+                    {/* Mapping over membersData to render each member using Card */}
+                    {membersData.map((member) => (
+                        // Rendering the Card component for each member
+                        <Card member={member} />
+                    ))}
                 </div>
-            {/* Column 3 */}
-                <div className="column">
-                    <div className="card">
-                        <div className="img-container">
-                            <img src="profile-img-1.png" alt="" />
-                        </div>
-                        <h3>David Warner</h3>
-                        <p>President</p>    
-                        <div className="icons">
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faEnvelope}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faTwitter}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faLinkedin}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faGithub}/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-            {/* Column 1 */}
-                <div className="column">
-                    <div className="card">
-                        <div className="img-container">
-                            <img src="profile-img-1.png" alt="" />
-                        </div>
-                        <h3>Luna Turner</h3>
-                        <p>Founder</p>
-                        <div className="icons">
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faEnvelope}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faTwitter}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faLinkedin}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faGithub}/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            {/* Column 2 */}
-                <div className="column">
-                    <div className="card">
-                        <div className="img-container">
-                            <img src="profile-img-1.png" alt="" />
-                        </div>
-                        <h3>James Clark</h3>
-                        <p>Developer</p>
-                        <div className="icons">
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faEnvelope}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faTwitter}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faLinkedin}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faGithub}/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            {/* Column 3 */}
-                <div className="column">
-                    <div className="card">
-                        <div className="img-container">
-                            <img src="profile-img-1.png" alt="" />
-                        </div>
-                        <h3>David Warner</h3>
-                        <p>President</p>    
-                        <div className="icons">
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faEnvelope}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faTwitter}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faLinkedin}/>
-                            </a>
-                            <a href="#" className="icon">
-                                <FontAwesomeIcon icon={faGithub}/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
-  )
+            </section>
+        </div>
+    )
 }
