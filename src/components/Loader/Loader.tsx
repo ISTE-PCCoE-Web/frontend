00a8abdaa9a4fd4242fb915080186c658f47cc2c
Sourav <sourav.97032@gmail.com>
2023-09-17 17:40:47 +0530

    Added loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,52 @@
+// import React from 'react'
+import { useState, CSSProperties } from "react";
+
+import { Grid } from 'react-loader-spinner'
+const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+
+export default function Loader() {
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#ffffff");
+
+  return (
+    <>
+{/* <Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+  /> */}
+{/* <Dna
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="dna-loading"
+  wrapperStyle={{}}
+  wrapperClass="dna-wrapper"
+/> */}
+{/* <FallingLines
+  color="#4fa94d"
+  width="100"
+  visible={true}
+  ariaLabel="falling-lines-loading"
+/> */}
+<Grid
+  height="80"
+  width="80"
+  color="#4fa94d"
+  ariaLabel="grid-loading"
+  radius="12.5"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+/>
+    </>
+  )
+}
