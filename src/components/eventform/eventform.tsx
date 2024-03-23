8f0a6d6224202807255b3eba5e0128fd0629206c
Sourav <sourav.97032@gmail.com>
2024-03-23 12:10:37 +0530

    added_event_form

---
+++ src/components/eventform/eventform.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const eventform = () => {
+  return (
+    <div>
+        <h1 className="text-center mt-8 font-bold text-4xl">Add Event</h1>
+        <form className='flex align-center justify-center w-full h-full flex-col'>
+            <div className='my-4'>
+        <label htmlFor="event_name" className="block text-gray-700">Event Name:</label>
+<input name='event_name' type="text" className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500" />
+</div>
+<div className='my-4'>
+        <label htmlFor="event_desc" className="block text-gray-700">Event Description:</label>
+<input name='event_desc' type="text" className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500" />
+</div>
+<div className='my-4'>
+        <label htmlFor="event_name" className="block text-gray-700">Date and Time for event:</label>
+<input name='event_name' type="datetime-local" className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500" />
+</div>
+
+<button className="p-4 mx-auto rounded-md font-bold text-white bg-green-500 hover:bg-green-600 "type='submit'>Add Event</button>
+        </form>
+
+    </div>
+  )
+}
+
+export default eventform
